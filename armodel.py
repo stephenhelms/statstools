62be4fce5910a31d254e37f43fd44736efd6781c
Stephen Helms <stephen.helms@gmail.com>
2014-11-29 13:06:39 +0100

    Updated some more of Antonio's code and added some model test plots

--- armodel.py
+++ armodel.py
@@ -5,6 +5,7 @@ import itertools
 import collections
 from scipy import stats
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 def tsToAutoregressiveForm(Y, p):
@@ -13,27 +14,28 @@ def tsToAutoregressiveForm(Y, p):
     are time points
     p is the max order
     Returns: Yf, the observations; Yp, the lagged predictors
-    Note: The first p rows of the output are set to masked so the output can be directly
-    compared with the input matrix Y.
+    Note: The first p rows of the output are set to masked so the output can
+    be directly compared with the input matrix Y.
     '''
-    # if a 1D time series is provided, make it 2D to avoid axis problems below
-    if len(Y.shape) == 1:
-        Y = Y[:, np.newaxis]
+    Y = ma.atleast_2d(Y)
     nObs, nVar = Y.shape
-    
+
     Yf = Y[p:, :]  # observed values in the fit
     Yp = ma.hstack(tuple(Y[p-pj:-pj, :] for pj in range(1, p+1)))
 
-    # because the lagged data loses the first p values, replace them with masked
-    lostObsf = ma.array(ma.zeros((p, nVar)), mask=True)  # placeholder for first p obs
+    # because the lagged data loses the first p values, replace with masked
+    lostObsf = ma.array(ma.zeros((p, nVar)), mask=True)
     Yf = ma.vstack((lostObsf, Yf))
-    lostObsp = ma.array(ma.zeros((p, nVar*p)), mask=True)  # placeholder for first p obs
+    lostObsp = ma.array(ma.zeros((p, nVar*p)), mask=True)
     Yp = ma.vstack((lostObsp, Yp))
     return Yf, Yp
 
 
 def fitARmodel(Y, p):
     '''
+    Fits an AR model of order p to the nObservation x nChannels
+    input time series Y.
+    Returns the 2D coefficient matrix W.
     '''
     # Get lagged vectors
     Yf, Yp = tsToAutoregressiveForm(Y, p)
@@ -46,13 +48,32 @@ def fitARmodel(Y, p):
 
 
 def predictARmodel(Y, W, p):
+    '''
+    Calculates the prediction of the AR model of order p
+    specified by W for the input data Y.
+    Returns the nObservations x nChannels prediction time series.
+    '''
     Yf, Yp = tsToAutoregressiveForm(Y, p)
     return ma.dot(Yp, W)
 
 
 def residualARmodel(Y, W, p):
+    '''
+    Calculates the residual for each time point for the AR model
+    from the input data Y using the model of order p specified by W.
+    Returns the nObservations x nChannels residual.
+    '''
     Yhat = predictARmodel(Y, W, p)
     return Y - Yhat
+
+
+def calculateR2(Y, eps):
+    '''
+    Calculates the coefficient of determination of the AR fit
+    from the input data Y and the residual eps.
+    Returns the R2 values for each channel.
+    '''
+    return 1. - eps.var(axis=0)/(Y-Y.mean(axis=0)).var(axis=0)
 
 
 def plotARfit(Y, p):
@@ -68,41 +89,68 @@ def plotARfit(Y, p):
     plt.show()
 
 
-def model_dynamics(Ap, Fs=1.):
+def plotARfitR2(Y, p):
+    p = np.atleast_1d(p)
+    w = 1./float(len(p)+2)  # bar width
+    colors = sns.color_palette('GnBu_d', len(p))
+    for i, pj in enumerate(p):
+        W = fitARmodel(Y, pj)
+        eps = residualARmodel(Y, W, pj)
+        R2 = calculateR2(Y, eps)
+        plt.bar(np.arange(0.5, Y.shape[1]+0.5)+float(i+1)*w, R2,
+                width=w, color=colors[i], edgecolor='None',
+                label='AR{0}'.format(str(pj)))
+    plt.xticks(xrange(1, Y.shape[1]+1))
+    plt.xlabel('Channel')
+    plt.ylabel('$R^2$')
+    plt.legend(loc=3, frameon=True)
+    plt.show()
+
+
+def model_dynamics(Wf, Fs=1.):
     '''
     Computes the damping times and periods associated with lag order p
-    Returns a tuple in which the first component is an array of the damping times and the second is an array of the periods
-    Ap is the parameter matrix for lag order p
+    Returns a tuple in which the first component is an array of the
+    damping times and the second is an array of the periods
+    Wf is the Frobenius norm parameter matrix for lag order p
     '''
-    l = np.linalg.eig(Ap)[0]
-    tau = -1/np.log(abs(l))
-    T=2.*np.pi/ma.abs(ma.angle(l))
+    l = np.linalg.eig(Wf)[0]
+    # calculate damping times from real part
+    tau = -np.sign(np.real(l))/np.log(np.abs(l))
+    # calculate oscillation period from imag part
+    T = 2.*np.pi/ma.abs(ma.angle(l))
+    # ignore oscilaltion periods for components that are ~purely real
     T[np.abs(np.imag(l)) < 1e-8] = np.inf
+    # convert to s if a sampling freq was provided
     return (tau/Fs, T/Fs)
 
 
-def trans_frobenius(Coef,dim,p):
+def trans_frobenius(W):
     '''
     Coef is the coefficient matrix in which we have vertically stacked dim*dim A matrices
+    This needs detailed comments Antonio
     '''
-    A=Coef
-    At_row=[]
-    for i in range(1,p+1):
-        At_row.append(A[4*(i-1):dim*i])
-    Ap=np.hstack(At_row)
-    Atr=np.zeros((dim*p,dim*p),complex)
-    for i in range(dim):
-        for j in range(dim*p):
-            Atr[i,j]=Ap[i,j]
-    i=dim
-    j=0
-    while j<dim*p:
-        while i<dim*p:
-            Atr[i,j]=1
-            j+=1
-            i+=1
-        j+=1
+    nVar = W.shape[1]
+    p = W.shape[0]/W.shape[1]
+
+    At_row = []
+    for i in range(1, p+1):
+        At_row.append(W[4*(i-1):nVar*i])
+    Ap = np.hstack(At_row)
+    Atr = np.zeros((nVar*p, nVar*p), complex)
+    for i in range(nVar):
+        for j in range(nVar*p):
+            Atr[i, j] = Ap[i, j]
+    i = nVar
+    j = 0
+    while j < nVar*p:
+        while i < nVar*p:
+            Atr[i, j] = 1
+            j += 1
+            i += 1
+        j += 1
     return Atr
+
 
 def f(List,i,p):
     x=[]
